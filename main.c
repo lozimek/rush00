d8f3e8bfd9b46b14be82f36cd5c7cb0af91106e9
Luka Ozimek Santi <lukozime@c1r2p8.42nice.fr>
2024-09-18 09:57:06 +0200

    finish

--- main.c
+++ main.c
@@ -6,15 +6,15 @@
 /*   By: lukozime <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 13:05:50 by lukozime          #+#    #+#             */
-/*   Updated: 2024/09/16 09:54:51 by lukozime         ###   ########.fr       */
+/*   Updated: 2024/09/18 09:56:43 by lukozime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void    rush(int x, int y);
-void    ft_print(int width, char first, char middle, char last);
+void	rush(int x, int y);
+void	ft_print(int width, char first, char middle, char last);
 void	ft_putchar(char c);
 
-int	main()
+int	main(void)
 {
 	rush(0, 65);
 	return (0);
